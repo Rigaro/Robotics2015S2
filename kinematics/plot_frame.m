460655fb04740a68fa02c1e6cbe40a9ab93f99f2
R C <chinr@student.unimelb.edu.au>
2015-08-20 01:01:54 +1000

    fixed plot override

--- kinematics/plot_frame.m
+++ kinematics/plot_frame.m
@@ -1,69 +1,83 @@
 function plot_frame()
 %plots the scaffolding and building
 
+Xconst = 0.19225;
+Yconst = 0.378375;
+Zconst1 = 0.043875;
+Zconst2 = 0.36;
+
 %top part of frame
-top_rect = [0.19225 0.378375 0.36;
-    -0.19225 0.378375 0.36;
-    -0.19225 -0.378375 0.36;
-    0.19225 -0.378375 0.36;
-    0.19225 0.378375 0.36];
+top_rect = [Xconst Yconst Zconst2;
+    -Xconst Yconst Zconst2;
+    -Xconst -Yconst Zconst2;
+    Xconst -Yconst Zconst2;
+    Xconst Yconst Zconst2];
 
 %bottom part of frame
-bot_rect = [0.19225 0.378375 0.043875;
-    -0.19225 0.378375 0.043875;
-    -0.19225 -0.378375 0.043875;
-    0.19225 -0.378375 0.043875;
-    0.19225 0.378375 0.043875];
+bot_rect = [Xconst Yconst Zconst1;
+    -Xconst Yconst Zconst1;
+    -Xconst -Yconst Zconst1;
+    Xconst -Yconst Zconst1;
+    Xconst Yconst Zconst1];
+
+%top centre bar
+bar = [Xconst 0 Zconst2;
+    -Xconst 0 Zconst2];
 
 %frame legs
-leg1 = [0.19225 0.378375 0.36;
-    0.19225 0.378375 0];
+leg1 = [Xconst Yconst Zconst2;
+    Xconst Yconst 0];
 
-leg2 = [-0.19225 0.378375 0.36;
-    -0.19225 0.378375 0];
+leg2 = [-Xconst Yconst Zconst2;
+    -Xconst Yconst 0];
 
-leg3 = [-0.19225 -0.378375 0.36;
-    -0.19225 -0.378375 0];
+leg3 = [-Xconst -Yconst Zconst2;
+    -Xconst -Yconst 0];
 
-leg4 = [0.19225 -0.378375 0.36;
-    0.19225 -0.378375 0];
+leg4 = [Xconst -Yconst Zconst2;
+    Xconst -Yconst 0];
 
-leg5 = [0.19225 0 0.36;
-    0.19225 0 0];
+leg5 = [Xconst 0 Zconst2;
+    Xconst 0 0];
 
-leg6 = [-0.19225 0 0.36;
-    -0.19225 0 0];
+leg6 = [-Xconst 0 Zconst2;
+    -Xconst 0 0];
+
+
+buildXconst = 0.105;
+buildYconst = 0.225;
+buildZconst = 0.21;
 
 %building faces
-top_face = [0.105 0.225 0.21;
-    -0.105 0.225 0.21;
-    -0.105 -0.225 0.21;
-    0.105 -0.225 0.21;
-    0.105 0.225 0.21];
+top_face = [buildXconst buildYconst buildZconst;
+    -buildXconst buildYconst buildZconst;
+    -buildXconst -buildYconst buildZconst;
+    buildXconst -buildYconst buildZconst;
+    buildXconst buildYconst buildZconst];
 
-east_face = [0.105 0.225 0.21;
-    0.105 -0.225 0.21;
-    0.105 -0.225 0;
-    0.105 0.225 0;
-    0.105 0.225 0.21];
+east_face = [buildXconst buildYconst buildZconst;
+    buildXconst -buildYconst buildZconst;
+    buildXconst -buildYconst 0;
+    buildXconst buildYconst 0;
+    buildXconst buildYconst buildZconst];
 
-west_face = [-0.105 0.225 0.21;
-    -0.105 -0.225 0.21;
-    -0.105 -0.225 0;
-    -0.105 0.225 0;
-    -0.105 0.225 0.21];
+west_face = [-buildXconst buildYconst buildZconst;
+    -buildXconst -buildYconst buildZconst;
+    -buildXconst -buildYconst 0;
+    -buildXconst buildYconst 0;
+    -buildXconst buildYconst buildZconst];
 
-north_face = [0.105 0.225 0.21;
-    -0.105 0.225 0.21;
-    -0.105 0.225 0;
-    0.105 0.225 0;
-    0.105 0.225 0.21];
+north_face = [buildXconst buildYconst buildZconst;
+    -buildXconst buildYconst buildZconst;
+    -buildXconst buildYconst 0;
+    buildXconst buildYconst 0;
+    buildXconst buildYconst buildZconst];
 
-south_face = [0.105 -0.225 0.21;
-    -0.105 -0.225 0.21;
-    -0.105 -0.225 0;
-    0.105 -0.225 0;
-    0.105 -0.225 0.21];
+south_face = [buildXconst -buildYconst buildZconst;
+    -buildXconst -buildYconst buildZconst;
+    -buildXconst -buildYconst 0;
+    buildXconst -buildYconst 0;
+    buildXconst -buildYconst buildZconst];
 
 %grasss
 ground = [0.6 0.6 0;
@@ -75,6 +89,7 @@ ground = [0.6 0.6 0;
 plot3(top_rect(:, 1), top_rect(:, 2), top_rect(:, 3), 'black', 'Linewidth', 3)
 hold on
 plot3(bot_rect(:, 1), bot_rect(:, 2), bot_rect(:, 3), 'black', 'Linewidth', 3)
+plot3(bar(:, 1), bar(:, 2), bar(:, 3), 'black', 'Linewidth', 3)
 plot3(leg1(:, 1), leg1(:, 2), leg1(:, 3), 'black', 'Linewidth', 3)
 plot3(leg2(:, 1), leg2(:, 2), leg2(:, 3), 'black', 'Linewidth', 3)
 plot3(leg3(:, 1), leg3(:, 2), leg3(:, 3), 'black', 'Linewidth', 3)
@@ -94,8 +109,8 @@ fill3(south_face(:, 1), south_face(:, 2), south_face(:, 3), grey)
 
 fill3(ground(:, 1), ground(:, 2), ground(:, 3), grass_green)
 
-%%For adjusting plot (already done in demo script)
-% daspect([1 1 1])
-% axis([-0.6, 0.6, -0.6, 0.6, 0, 1])
+%For adjusting plot (already done in demo script)
+daspect([1 1 1])
+axis([-0.6, 0.6, -0.6, 0.6, 0, 1])
 %'orthogonal' view
-% view(3);
+view(3);
